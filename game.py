6a61efe333ac218937c3b2864b4c2422e93e2bd2
Mousumi Sen <98817832+MousumiAria@users.noreply.github.com>
2022-02-23 15:52:29 +0100

    Updated files

--- game.py
+++
@@ -1,7 +0,0 @@
-class Board:
-    def __init__(self, players:int,turn_count:int,active_cards:int,history_cards:int):
-        """Constructor our class"""
-        self.players = players
-        self.turn_count = turn_count
-        self.active_cards = active_cards
-        self.history_cards = history_cards
